b2c320a05e5a60e76e7f343fc2f6a0f7c5394cb0
Angel <aiglesiaf@hotmail.com>
2017-11-27 17:21:23 +0100

    UC67 Laying out geometry

---
+++ BuildingEscape 4.18/Source/BuildingEscape/PositionReport.cpp
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "PositionReport.h"
+#include "GameFramework/Actor.h"
+
+
+
+// Sets default values for this component's properties
+UPositionReport::UPositionReport()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UPositionReport::BeginPlay()
+{
+	Super::BeginPlay();
+	const FString ObjectName = GetOwner()->GetName();
+	const FString ObjectPosition = GetOwner()->GetActorTransform().ToString();
+	UE_LOG(LogTemp, Warning, TEXT("The Position Report has been started in %s in position %s"), *ObjectName, *ObjectPosition);
+	
+}
+
+
+// Called every frame
+void UPositionReport::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
